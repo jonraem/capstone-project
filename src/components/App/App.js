e2292b02c664d1dd3b5c1d7343ea7c61c394e972
Joni Rämö <joni.ramo@nordsoftware.com>
2017-05-30 11:17:48 +0300

    Style the header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
+
+import { createBucket, copyObject } from '../../utils/aws';
 import logo from '../../assets/logo.svg';
 import './App.css';
 
-import { createBucket, copyObject } from '../../utils/aws';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className='header-logo'>{'VSS'}</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <button onClick={() => createBucket()}>Create Bucket in S3</button>
         <button onClick={() => copyObject()}>Copy picture from S3</button>
       </div>
