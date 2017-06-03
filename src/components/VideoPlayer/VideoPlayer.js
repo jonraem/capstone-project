564490e9a91b1544e9a70f0c2fe2483399a56cdf
Joni Rämö <joni.ramo@nordsoftware.com>
2017-06-03 13:47:58 +0300

    Add VideoPlayer

---
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class VideoPlayer extends Component {
+  render() {
+    return (
+      <div className="VideoPlayer">
+        <p>Hello.</p>
+      </div>
+    );
+  }
+}
+
+export default VideoPlayer;
