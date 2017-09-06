b033d2e811855b7ed7ae1f8e6ca35bded4cd60df
jonraem <joni.ramo.91@gmail.com>
2017-09-06 14:47:14 +0300

    Add Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>This is the VSS Video Surveillance System.</h1>
+        <p>The latest technology in home surveillance systems. Try it and believe it.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
