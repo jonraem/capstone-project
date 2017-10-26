bfe99ff2eaf573f5176c2d71185932af15b88880
jonraem <joni.ramo.91@gmail.com>
2017-10-26 22:20:41 +0300

    Add iframe to app

--- src/components/StreamPlayer/index.js
+++ src/components/StreamPlayer/index.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 
 import './index.css';
 
+const iframeUrl = process.env.REACT_APP_IFRAME_URL;
+
 class StreamPlayer extends Component {
   render() {
     return (
       <div className="StreamPlayer">
         <div className="StreamPlayer-view">
           {/* <img alt='Stream Thumbnail' src={require('../../assets/dog.png')} className="thumbnail" /> */}
-          <img alt='Stream Thumbnail' src={require('../../assets/dog-tennis-olympics.jpg')} className="thumbnail" />
-          <img alt='Tennis ball' src={require('../../assets/tennis-ball.png')} className="tennis-ball" />
+          {/* <img alt='Stream Thumbnail' src={require('../../assets/dog-tennis-olympics.jpg')} className="thumbnail" /> */}
+          {/* <img alt='Tennis ball' src={require('../../assets/tennis-ball.png')} className="tennis-ball" /> */}
+          <iframe src={iframeUrl} width="450" height="350">
+            <p>Your browser does not support iframes.</p>
+          </iframe>
         </div>
-        <p>Not yet implemented.</p>
       </div>
     );
   }
